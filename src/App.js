f015c465af89843503826c07bee513b6bc9dfcc7
ASchwad <a.schoenenwald@gmx.de>
2021-10-11 18:25:35 +0200

    Add cube example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas, useFrame } from "react-three-fiber";
+import { softShadows, MeshWobbleMaterial, OrbitControls } from "drei";
+import { useSpring, a } from "react-spring/three";
+import "./App.scss";
+import { useRef, useState } from 'react';
+import React from 'react'
+
+// soft Shadows
+softShadows();
+
+const SpinningMesh = ({ position, color, speed, args, setShowBoxGame }) => {
+  //ref to target the mesh
+  const mesh = useRef();
+
+  //useFrame allows us to re-render/update rotation on each frame
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  //Basic expand state
+  const [expand, setExpand] = useState(false);
+  // React spring expand animation
+  const props = useSpring({
+    scale: expand ? [2,2,2] : [1, 1, 1],
+  });
+
+  let factor = expand ? 0.8 : 0;
+  
+  return (
+    <a.mesh
+      position={position}
+      ref={mesh}
+      onClick={() => setShowBoxGame(true)}
+      // onClick={() => setExpand(!expand)}
+      scale={props.scale}
+      castShadow>
+      <boxBufferGeometry attach='geometry' args={args} />
+      <MeshWobbleMaterial
+        color={color}
+        speed={speed}
+        attach='material'
+        factor={factor}
+      />
+    </a.mesh>
+  );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const [showBoxGame, setShowBoxGame] = useState(false);
+  const Header = () => {
+    return (
+      <header>
+        <div className='logo'>
+          <span>REACT THREE FIBER</span>
+        </div>
+        <div className='episode'>
+          <span>EP. 1</span>
+        </div>
+        <button onMouseDown={() => setShowBoxGame(true)}>BOX GAME</button>
       </header>
-    </div>
-  );
+    );
+  };
+
+  return (
+    <>
+      <Header />
+      <Canvas
+        colorManagement
+        shadowMap
+        camera={{ position: [-5, 4, 10], fov: 80 }}>
+        {/* This light makes things look pretty */}
+        <ambientLight intensity={0.3} />
+        {/* Our main source of light, also casting our shadow */}
+        <directionalLight
+          castShadow
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        {/* A light to help illumnate the spinning boxes */}
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <group>
+          {/* This mesh is the plane (The floor) */}
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -3, 0]}
+            receiveShadow>
+            <planeBufferGeometry attach='geometry' args={[50, 50]} />
+            <shadowMaterial attach='material' opacity={0.3} />
+          </mesh>
+          <SpinningMesh
+            position={[0, 1, 0]}
+            color='lightblue'
+            args={[3, 2, 1]}
+            speed={2}
+            setShowBoxGame={() => setShowBoxGame(true)}
+          />
+          <SpinningMesh position={[-2, 1, -5]} color='pink' speed={6} />
+          <SpinningMesh position={[5, 1, -2]} color='pink' speed={6} />
+        </group>
+        {/* Allows us to move the canvas around for different prespectives */}
+        <OrbitControls />
+      </Canvas>
+    </>
+  )
 }
 
 export default App;
